9911b1bfc4ca3e54ee3baf88f003aa0e77d9445b
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2024-09-25 16:27:55 +0900

    chat+
    채팅 기능 보강+세션 연동

--- app.js
+++ app.js
@@ -17,31 +17,21 @@ port = 3000
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.use(session({
+const sessionMiddleware = session({
     secret: 'my-secret-key',  // 세션 암호화를 위한 비밀 키
     resave: false,            // 세션이 수정되지 않아도 다시 저장할지 여부
     saveUninitialized: true,  // 초기화되지 않은 세션도 저장할지 여부
     cookie: { 
         maxAge: 60000         // 쿠키의 수명 (밀리초 단위)
     }
-}));
+})
 
+app.use(sessionMiddleware);
 
-// app.get('/chat', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'templates', 'chat.html')); // 클라이언트 HTML 파일 제공
-// });
+io.use((socket, next) => {      //소켓과 세션 연동
+    sessionMiddleware(socket.request, {}, next)
+})
 
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
-//     socket.on('chat', (msg) => {
-//         console.log('message:', msg);
-//         io.emit('chat', msg);
-//     })
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected:', socket.id)
-//     })
-// })
 
 app.use(express.static('templates'));
 app.use(express.static('public'))
@@ -75,10 +65,19 @@ app.get('/chat', (req, res) => {
 })
 
 io.on('connection', function(socket) { //왜 안되는거야?
+    const session = socket.request.session;     //현재 소켓의 세션 정보 가져오기
+    console.log("user:", session.nickname)   //세션 로그인한 유저 아이디 출력
     console.log("접속됨");
 
     socket.on('send', function(data) {
-        console.log('전달된 메시지:', data.msg)
+        if (!session.nickname) session.nickname = 'anonymous'
+        console.log('전달된 메시지:', data)
+        console.log('사용자:', session.nickname)
+        io.emit('update', {
+            uid: session.nickname,
+            msg: data
+        })
+        //보낸 메시지 브로드캐스트
     })
     
     socket.on('disconnect', function() {
--- public/chat.js
+++ public/chat.js
@@ -1,38 +1,21 @@
 var socket = io()
 
 socket.on('connect', function() {
-    var input = document.getElementById('input')
-    input.value = 'connected'
-    console.log('con')
+        console.log('con')
 })
 
-function send() {
-    var msg = document.getElementById('input').value
-    document.getElementById('input').value = ''
-    socket.emit('send', {msg: msg})
-    console.log("SEND!!!")
-}
 
-/*
-    <script>
-        const socket = io();
+document.getElementById('form').addEventListener('submit', function(e) {        //메시지를 보낼 때
+    e.preventDefault(); // 기본 폼 제출 방지
+    var input = document.getElementById('input')
+    console.log('msg:', input.value)
+    socket.emit('send', input.value)
+    input.value = ''
+})
 
-        const form = document.getElementById('form');
-        const input = document.getElementById('input');
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            if (input.value) {
-                socket.emit('send', input.value); // 메시지를 소켓으로 전송
-                input.value = ''; // 입력란 초기화
-            }
-        });
-
-        socket.on('send', (msg) => {
-            const item = document.createElement('li'); // 새로운 리스트 아이템 생성
-            item.textContent = msg; // 메시지 설정
-            document.getElementById('messages').appendChild(item); // 리스트에 추가
-            window.scrollTo(0, document.body.scrollHeight); // 스크롤 하단으로 이동
-        });
-    </script>
-*/
+socket.on('update', function(data) {     //(본인 포함) 유저가 메세지를 보낸 것을 확인했을 때
+    const li = document.createElement('li');   //리스트 생성
+    li.textContent = `${data.uid}: ${data.msg}`    //메시지 내용 추가
+    document.getElementById('messages').appendChild(li);     //리스트에 추가
+    window.scrollTo(0, document.body.scrollHeight)      // 스크롤 하단으로 이동
+})
