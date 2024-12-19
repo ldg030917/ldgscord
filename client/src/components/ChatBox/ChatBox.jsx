30c108701196297a95dc5606ae1bd07535698766
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2024-12-19 16:45:38 +0900

    Feat: chat box 디자인 추가
    챗 박스 프론트 작업 완료

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { FaDiscord } from "react-icons/fa";
 import './ChatBox.css';
 
 function ChatBox() {
   const [message, setMessage] = useState('');
+  const textareaRef = useRef(null);
+
+  const adjustHeight = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
+  };
 
   const handleInputChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && event.shiftKey) {
+      return;
+    }
+
+    if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault(); // 기본 엔터키 기능 (줄바꿈) 방지
       handleSubmit(); // 폼 제출
     }
@@ -20,17 +34,23 @@ function ChatBox() {
     console.log('폼 제출:', message);
     setMessage(''); // 메시지 초기화 (옵션)
   };
-
+  
+  useEffect(() => {
+    adjustHeight();
+  }, [message]);
+  
   return(
     <form className="chat-box" onSubmit={(e) => e.preventDefault()}>
+      <button className="chat-icon-button"><FaDiscord size={24} /></button>
       <textarea 
+        ref={textareaRef}
         className="chat-input"
         value={message}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
         rows="1" 
         autocomplete="off" 
-        required
+        spellCheck="false"
       />
       <button style={{ display: 'none' }} type="submit" onClick={handleSubmit}/>
     </form>
