f4924cc827cff5204d381cbe3abfec896a73b314
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-14 23:35:09 +0900

    TEST Menu
    context menu 연결 작업

--- client/src/components/MessageBox/MessageBox.jsx
+++ client/src/components/MessageBox/MessageBox.jsx
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import WIP from '../WIP';
+import ContextMenu from '../ContextMenu/ContextMenu';
 import './MessageBox.css';
 
-function MessageBox({data}) {
-
+function MessageBox({ data }) {
+  const messageBoxRef = useRef(null);
   return (
-    <div className='message-box'>
+    <div className='message-box' ref={messageBoxRef}>
+      <ContextMenu ParentRef={messageBoxRef} />
       <div className='profile-img'>
         {data.user_id}
         <WIP></WIP>
       </div>
       <div className='text-area'>
-        <div>
-          {data.created_at}
-        </div>
-        <div>
-          {data.content}
-        </div>
+        <div>{data.created_at}</div>
+        <div>{data.content}</div>
       </div>
     </div>
   );
