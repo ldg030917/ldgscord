ca2d815935bce99ec16fd0fc4bc6d581d2ee3c12
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-21 01:17:22 +0900

    Update serverRoutes.js

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const serverController = require('../controllers/serverController');
+const channelController = require('../controllers/channelController')
 const router = express.Router();
 
 
@@ -7,7 +8,7 @@ router.get('/servers', serverController.getUserServers);
 router.post('/servers', serverController.createServer);
 
 router.get('/servers/:server_id/channels', serverController.getServerChannels);
-//router.get('/servers/:server_id/channels');
+router.post('/servers/:server_id/channels', channelController.createChannel);
 
 router.get('/servers/:server_id/members', serverController.getUsers);
 router.post('/servers/:server_id/members', serverController.joinUserToServer);
