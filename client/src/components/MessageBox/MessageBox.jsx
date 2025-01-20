38ddf9269d1b694a723bcad7d89cd75873459f71
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-21 01:10:10 +0900

    FEAT & REFACTOR MessageBox
    css를 styled-component 방식으로 고침, css와 코드 조금 수정

--- client/src/components/MessageBox/MessageBox.jsx
+++ client/src/components/MessageBox/MessageBox.jsx
@@ -1,22 +1,67 @@
 import React, { useState, useEffect, useRef } from 'react';
 import WIP from '../WIP';
 import ContextMenu from '../ContextMenu/ContextMenu';
-import './MessageBox.css';
+import { formatDate } from '../../services/utils';
+import styled from 'styled-components';
 
-function MessageBox({ data }) {
+const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: white;
+  margin-top: 12px;
+`;
+
+const ProfileImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 40px;
+  width: 40px;
+  background-color: var(--blue-button-color);
+  border-radius: 50%;
+  margin: 10px;
+`;
+const TextArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Area1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-end;
+`;
+
+const Name = styled.div`
+  color: mintcream;
+  margin-right: 10px;
+`;
+
+const Date = styled.p`
+  font-size: 12px;
+  margin: 0;
+`;
+
+const MessageBox = ({ data }) => {
   const messageBoxRef = useRef(null);
   return (
-    <div className='message-box' ref={messageBoxRef}>
+    <Box ref={messageBoxRef}>
       <ContextMenu ParentRef={messageBoxRef} children={data.content} />
-      <div className='profile-img'>
-        {data.user_id}
-        <WIP></WIP>
-      </div>
-      <div className='text-area'>
-        <div>{data.created_at}</div>
-        <div>{data.content}</div>
-      </div>
-    </div>
+      <ProfileImg>
+        <WIP size={'20'}></WIP>
+      </ProfileImg>
+      <TextArea>
+        <Area1>
+          <Name>{data.user_id}</Name>
+          <Date>{formatDate(data.created_at)}</Date>
+        </Area1>
+        <>
+          <div>{data.content}</div>
+        </>
+      </TextArea>
+    </Box>
   );
 };
 
