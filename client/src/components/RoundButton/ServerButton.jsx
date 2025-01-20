b76f4c8dd711cf5f950056ce9e0f83d8d18088b0
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-20 18:42:56 +0900

    Update ServerButton.jsx

--- client/src/components/RoundButton/ServerButton.jsx
+++ client/src/components/RoundButton/ServerButton.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RoundButton from './RoundButton';
+import ContextMenu from '../ContextMenu/ContextMenu';
 
 function ServerButton({ server }) {
   const navigate = useNavigate();
+  const serverButtonRef = useRef(null);
 
   const handleClick = () => {
     navigate(`/channels/${server.id}`);
   };
 
   return (
-    <RoundButton onClick={handleClick}>
+    <RoundButton ref={serverButtonRef} onClick={handleClick} >
+      <ContextMenu ParentRef={serverButtonRef} />
       {server.name}
     </RoundButton>
   );
