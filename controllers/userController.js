cd70cb0fb40bc857b436dbb89eb67c554ccd8238
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-23 15:09:00 +0900

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 //사용자에 대한 요청 처리
 const userService = require('../services/userService');
+const friendModel = require('../models/Friend');
 
 const register = async (req, res) => {
     const id = req.body.id;
@@ -27,6 +28,39 @@ const login = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
-}
+};
 
-module.exports = { register, login };
+const sendFriendRequest = async (req, res) => {
+    const id = req.user.id;
+    const to_user_id = req.body.id;
+    try {
+        await friendModel.createFriendRequest(to_user_id, id, 0);
+        res.status(200).json({});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const acceptFriendRequest = async (req, res) => {
+    const id = req.user.id;
+    const from_user_id = req.body.id;
+    try {
+        await friendModel.createFriendship(id, from_user_id);
+        //dm용 채널 하나 만들기
+        res.status(200).json({});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+const getFriends = async (req, res) => {
+    const id = req.user.id;
+    try {
+        const friends = await friendModel.getFriends(id);
+        res.status(200).json({ friends: friends });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = { register, login, getFriends, sendFriendRequest, acceptFriendRequest };
