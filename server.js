6d301789788354dfcc378c4fb84542bcba09d0cf
ldg030917 <127109929+ldg030917@users.noreply.github.com>
2025-01-23 17:51:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app)
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: ["http://3.25.29.33:5000", "http://localhost:3000"],
     credentials: true,
 }
 app.use(cors(corsOptions));
